3a579730a469f8cce853ca003ecc48c0ec1865d1
Sabareesan Thirukumaran <thirukumaransabareesan18@gmail.com>
2025-07-16 13:55:06 +0100

    Continued to add file checks and beginning to add image file size dimension checks

--- scripts/dataset_summary.py
+++ scripts/dataset_summary.py
@@ -33,4 +33,63 @@ if os.path.exists(realImages_fullPath):
 
 # All Image Count
 if totalImages == 300:
-    print("All Images Collected")
+    
+    #Checking image & annotation compatability
+    RealannotationPath = os.path.join("data","annotations","real")
+    VirtualannotationPath = os.path.join("data","annotations","virtual")
+
+    if len(os.listdir(RealannotationPath)) == countReal and len(os.listdir(VirtualannotationPath)) == countVirtual:
+        count = 0
+        
+        # Checking all REAL compatability
+        real_image_basenames = set(os.path.splitext(f)[0] for f in os.listdir(realImages_fullPath) if os.path.isfile(os.path.join(realImages_fullPath, f)))
+        real_annotations_basenames = set(os.path.splitext(f)[0] for f in os.listdir(RealannotationPath) if os.path.isfile(os.path.join(RealannotationPath, f)))
+
+        if real_image_basenames == real_annotations_basenames:
+            print("All REAL annotations are in equal to Images")
+        else:
+            print("There is a mismatch (REAL)")
+
+        # Checking all VIRTUAL compatability
+        virtual_image_basenames = set(os.path.splitext(f)[0] for f in os.listdir(virtualImages_fullPath) if os.path.isfile(os.path.join(virtualImages_fullPath, f)))
+        virtual_annotations_basenames = set(os.path.splitext(f)[0] for f in os.listdir(VirtualannotationPath) if os.path.isfile(os.path.join(VirtualannotationPath, f)))
+
+        if virtual_image_basenames == virtual_annotations_basenames:
+            print("All VIRTUAL annotations are in equal to Images")
+        else:
+            print("There is a mismatch (VIRTUAL)")
+
+# Checking Image Corruption Validity
+
+def checkImageCorruption(path):
+    return cv2.imread(path) is None
+
+# 1 - Real Images
+realCorruptionCount = 0
+for images in os.listdir(realImages_fullPath):
+    fullImagePath = os.path.join(realImages_fullPath,images)
+    if checkImageCorruption(fullImagePath):
+        print(f"{fullImagePath} is corrupted.")
+        realCorruptionCount += 1
+
+if realCorruptionCount == len(os.listdir(realImages_fullPath)):
+    print("All Images are NOT corrupted")
+
+# 2 - Virtual Images
+virtualCorruptionCount = 0
+for images in os.listdir(virtualImages_fullPath):
+    fullImagePath = os.path.join(virtualImages_fullPath,images)
+    if checkImageCorruption(fullImagePath):
+        print(f"{fullImagePath} is corrupted.")
+        virtualCorruptionCount += 1
+
+if virtualCorruptionCount == len(os.listdir(virtualImages_fullPath)):
+    print("All Images are NOT corrupted")
+
+# Checking Image dimension Validity
+
+def checkImageSize(path):
+    img = cv2.imread(path)
+    width, height = img.shape[:2]
+    return width <= 0 or height <= 0 or width > 1920 or height > 1920
+
